54bbdcc478f2c0cad69bff7a870ad4638350b3c8
Nathan.TienYou <nathan.tienyou@viacesi.fr>
2022-06-27 15:31:42 +0200

    Typescript Service Worker (to test)

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -78,3 +78,16 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+self.addEventListener("push", (e) => {
+  const data = e.data!.json();
+  console.log(data)
+  self.registration.showNotification(
+    data.title, // title of the notification
+    {
+      body: data.body, //the body of the push notification
+      image:
+        data.image,
+      icon: data.icon, // icon
+    }
+  );
+});
