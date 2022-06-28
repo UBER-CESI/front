fcbb5a4efe2e66d9aff2e9021aa4b9057b2a50a0
TienYou-Nathan <nathan.tienyou@viacesi.fr>
2022-06-28 22:26:02 +0200

    debug notifications

--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -1,3 +1,13 @@
+import axios from "axios"
+
+async function login(){
+  return instance.post("login", {"email":"c@c.com", password:"password"})
+}
+
+const instance = axios.create({withCredentials:true, baseURL:"https://ubernathan.sleepycat.date", headers: {
+  'Content-Type': 'application/json',
+}})
+
 function urlBase64ToUint8Array (base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding)
@@ -14,34 +24,36 @@ function urlBase64ToUint8Array (base64String: string): Uint8Array {
 }
 
 async function setupServiceWorker (url: string) {
+  console.log("oui1")
+  console.log(await login())
+  console.log("oui2")
   // register service worker
   const register = await navigator.serviceWorker.register('./pushWorker.js', {
     scope: '/'
   })
+  console.log("oui5")
   await navigator.serviceWorker.ready
+  console.log("oui6")
   // register push
-  const subscription = await register.pushManager.subscribe({
+  await register.pushManager.getSubscription().then(sub => sub?.unsubscribe()).finally()
+  const subscription =   (await register.pushManager.subscribe({
     userVisibleOnly: true,
-
     // public vapid key
-    applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
-  })
+    //applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+    applicationServerKey:publicVapidKey
+  }))
+  console.log("subscription: ",subscription)
   // Send push notification
-  await fetch(url + '/subscribe', {
-    method: 'POST',
-    body: JSON.stringify({subscription}),
-    headers: {
-      'content-type': 'application/json'
-    }
-  }).then(e=>console.log(e))
+  await instance.post('/notifications/subscribe', {subscription}
+  )
 }
 
 const publicVapidKey =
-  'BKGyLVKB2CavcJxo59dc8nZ-Kv_toI2lcbQ3fpWslaaQM3m9Nd9ozGmbAq8FOPLqFYxswrYtoEbbFrccp3nQY6U'
+  'BMBYjGF0wds0nTPo7S-3E1KVyjuaW8XhJrL2p-yvkm1Pk3qeHovUZ7OaRCDfqRhe6hW8lg7qBl6KynM3dlrNt8c'
 
 export function setupNotifications (url: string) {
   if ('serviceWorker' in navigator) {
-    setupServiceWorker(url).catch((err) => console.error(err))
+    setupServiceWorker(url)
   }else{
     console.error("Service Workers are not available on this platform")
   }
